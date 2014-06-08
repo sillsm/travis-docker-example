36910b2d3c025642eb40c7dfccca2401e5459ca1
Max Sills <sillsm@gmail.com>
2014-06-07 19:13:45 -0700

    Update uml.sh

--- uml.sh
+++ uml.sh
@@ -6,9 +6,18 @@ set -e
 # Kill background processes on exit
 trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
 
+uname -a 
+echo that was the current kernel
+sudo apt-get install debootstrap 
+sudo apt-get install schroot
+sudo mkdir -p /newmount
+sudo debootstrap --variant=buildd trusty /newmount http://archive.ubuntu.com/ubuntu/
+sudo mount -o bind /proc /newmount/proc
+sudo cp /etc/resolv.conf /newmount/etc/resolv.conf
+sudo chroot /newmount uname -a
 # Start docker daemon
-docker -d &
-sleep 10
+#docker -d &
+#sleep 10
 
 # Use docker
-docker run ubuntu /bin/echo hello world
+#docker run ubuntu /bin/echo hello world
